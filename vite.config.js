1b595cbd08db529d6bb021aa63553751c5281820
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-12-05 17:04:14 +0800

    fix: vite base name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/Vite-vue3-basic-environment/"
+  base: "/Vue3-vite-tailwind-jit-mode/"
 })
